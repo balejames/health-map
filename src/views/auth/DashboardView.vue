dcfb8ad8125ba76447cc9a1edc34f9df023e6385
JamesBale <Elabjames@gmail.com>
2025-04-07 16:13:30 +0800

    added Lorem

--- src/views/auth/DashboardView.vue
+++ src/views/auth/DashboardView.vue
@@ -1,3 +1,9 @@
 <script setup></script>
-<template></template>
+<template>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci animi aut odit nisi unde, vel
+    doloribus, voluptatibus dolore accusamus dolores nam, quia quod sequi autem quaerat obcaecati
+    quidem accusantium minima.
+  </p>
+</template>
 <style></style>
