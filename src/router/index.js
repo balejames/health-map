083d646f0ce9898e8274433a65da319e5a1b7759
balejames <Elabjames@gmail.com>
2025-04-21 21:51:34 +0800

    fix .env

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,7 @@ import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import DashboardView from '@/views/auth/DashboardView.vue'
 import MapView from '@/views/auth/MapView.vue'
-import Viewer from '@/views/auth/ViewerDashboard.vue'
-
-
+import ViewerView from '@/views/auth/ViewerDashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,7 +35,7 @@ const router = createRouter({
     {
       path: '/viewer',
       name: 'Viewer',
-      component: Viewer,
+      component: ViewerView,
     },
   ],
 })
